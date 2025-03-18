89791860488b97c77868f8fb8600e9afec3a67e0
Tausif Ahmed <tausif.ritu1@gmail.com>
2025-03-18 14:08:28 +0600

    all are done

--- src/components/cards/game-card.tsx
+++ src/components/cards/game-card.tsx
@@ -2,7 +2,7 @@ import { ArrowUpRight } from "lucide-react";
 export const GameCard = () => {
   return (
     // TODO: please add your dynamic image based on the key(image will change key like : /images/posters/poster-${key}.jpg) 
-    <div className="w-full h-[540px] bg-black bg-no-repeat bg rounded-lg bg-cover bg-center flex items-end -tracking-wider bg-[url(/images/posters/poster-1.jpg)]">
+    <div className="w-full h-[540px] bg-black bg-no-repeat bg rounded-lg bg-cover bg-center flex items-end -tracking-wider bg-[url(/images/posters/poster-2.jpg)]">
       <div className="text-white w-full">
         <div className="px-8">
           <p>12 Feb 2024</p>
