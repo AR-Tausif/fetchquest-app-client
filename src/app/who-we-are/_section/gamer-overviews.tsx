25f5e25996b69bcbe041e6c2c742be5b2350e449
Tausif Ahmed <tausif.ritu1@gmail.com>
2025-03-18 13:40:23 +0600

    changes all cnd links to static images for who we are page

--- src/app/who-we-are/_section/gamer-overviews.tsx
+++ src/app/who-we-are/_section/gamer-overviews.tsx
@@ -17,8 +17,7 @@ const gamesOverviews: TSingleGameOverview[] = [
       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum, sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.|
 
       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum, sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium`,
-    photo:
-      "https://s3-alpha-sig.figma.com/img/0e1d/d7cb/3e99122e66a070b6b5cc85a573077dc2?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Oa-3OTjsDnHYiVxGKmnVNKrbVYWZW3RYnAqNOerTppEmk789OHol2Nr07oIcVTdZUzs3wm1b3NVCTQMNisXsFG2pIGQKe84078l~2asjXG3-TXi0p2VBumuaXnkjh-AZCThETOD3wBzPiT5TMY0LC9kQIqs6scyJZmMQi~UGFqAXcvplR2U7t~r3I3cGjQjr82qZnK-4A~Lu6kH8bs3qsPUdj851WcMuIBiTx-VTfhsD2ZoCtYjnUjTUO25Z6UhO34aLs~j-rePSmGnQQdfLRTbQL2wP-0b7zKMJ6a~M~5Y3f3zXDhZHvsVnEo7E7BRQB9XFcJrfxtajGYs4ktw5HQ__",
+    photo: "/images/pages/who-we-are/section-four/tab-img/tab-1.jpg",
   },
   // Adding additional objects based on the repeated images in the HTML
   {
@@ -26,16 +25,14 @@ const gamesOverviews: TSingleGameOverview[] = [
     name: "TANJEROO KUGASAKI",
     content:
       "Additional description could go here for the second image instance.",
-    photo:
-      "https://s3-alpha-sig.figma.com/img/0e1d/d7cb/3e99122e66a070b6b5cc85a573077dc2?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Oa-3OTjsDnHYiVxGKmnVNKrbVYWZW3RYnAqNOerTppEmk789OHol2Nr07oIcVTdZUzs3wm1b3NVCTQMNisXsFG2pIGQKe84078l~2asjXG3-TXi0p2VBumuaXnkjh-AZCThETOD3wBzPiT5TMY0LC9kQIqs6scyJZmMQi~UGFqAXcvplR2U7t~r3I3cGjQjr82qZnK-4A~Lu6kH8bs3qsPUdj851WcMuIBiTx-VTfhsD2ZoCtYjnUjTUO25Z6UhO34aLs~j-rePSmGnQQdfLRTbQL2wP-0b7zKMJ6a~M~5Y3f3zXDhZHvsVnEo7E7BRQB9XFcJrfxtajGYs4ktw5HQ__",
+    photo: "/images/pages/who-we-are/section-four/tab-img/tab-1.jpg",
   },
   {
     id: 3,
     name: "KAKASHI UCHIHA",
     content:
       "Additional description could go here for the third image instance.",
-    photo:
-      "https://s3-alpha-sig.figma.com/img/0e1d/d7cb/3e99122e66a070b6b5cc85a573077dc2?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Oa-3OTjsDnHYiVxGKmnVNKrbVYWZW3RYnAqNOerTppEmk789OHol2Nr07oIcVTdZUzs3wm1b3NVCTQMNisXsFG2pIGQKe84078l~2asjXG3-TXi0p2VBumuaXnkjh-AZCThETOD3wBzPiT5TMY0LC9kQIqs6scyJZmMQi~UGFqAXcvplR2U7t~r3I3cGjQjr82qZnK-4A~Lu6kH8bs3qsPUdj851WcMuIBiTx-VTfhsD2ZoCtYjnUjTUO25Z6UhO34aLs~j-rePSmGnQQdfLRTbQL2wP-0b7zKMJ6a~M~5Y3f3zXDhZHvsVnEo7E7BRQB9XFcJrfxtajGYs4ktw5HQ__",
+    photo: "/images/pages/who-we-are/section-four/tab-img/tab-1.jpg",
   },
   {
     id: 4,
@@ -43,17 +40,16 @@ const gamesOverviews: TSingleGameOverview[] = [
     content: `Naruto follows the story of Naruto Uzumaki, a young ninja with dreams of becoming the Hokage, the leader of his village, despite being shunned for having the Nine-Tailed Fox spirit sealed within him. The series starts with him as a mischievous, loud kid who’s terrible at ninja skills but grows into a powerful, inspiring figure through grit, friendship, and a lot of battles. It’s split into two main parts: the original Naruto, focusing on his childhood, and Naruto: Shippuden, which picks up after a time skip as he matures and faces bigger threats. |
 
     The manga ran from 1999 to 2014, with 72 volumes, and the anime adaptation has hundreds of episodes—Naruto with 220, and Shippuden with 500. It’s packed with ninja action, elemental jutsu (techniques), and themes about perseverance, loyalty, and redemption. Key characters include Sasuke Uchiha, his brooding rival, and Sakura Haruno, their teammate. The world’s got a mix of feudal vibes and supernatural flair, with villages like the Hidden Leaf and clans wielding unique abilities.`,
-    photo:
-      "https://s3-alpha-sig.figma.com/img/0e1d/d7cb/3e99122e66a070b6b5cc85a573077dc2?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Oa-3OTjsDnHYiVxGKmnVNKrbVYWZW3RYnAqNOerTppEmk789OHol2Nr07oIcVTdZUzs3wm1b3NVCTQMNisXsFG2pIGQKe84078l~2asjXG3-TXi0p2VBumuaXnkjh-AZCThETOD3wBzPiT5TMY0LC9kQIqs6scyJZmMQi~UGFqAXcvplR2U7t~r3I3cGjQjr82qZnK-4A~Lu6kH8bs3qsPUdj851WcMuIBiTx-VTfhsD2ZoCtYjnUjTUO25Z6UhO34aLs~j-rePSmGnQQdfLRTbQL2wP-0b7zKMJ6a~M~5Y3f3zXDhZHvsVnEo7E7BRQB9XFcJrfxtajGYs4ktw5HQ__",
+    photo: "/images/pages/who-we-are/section-four/tab-img/tab-1.jpg",
   },
 ];
 
 export const GamerOverviewSection = () => {
   const afterBgUrl =
-    "https://s3-alpha-sig.figma.com/img/d3ed/09f7/252004e28d933592d719d0fca45e5787?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=H0ihIPUscpg9fFeNj~ZI2LnKPv-zvtUsP88b6G4tmrBXkp-4nJXX-pFJ5jAv4PwzwTn2ItkCj99lNhDz9IvVMqdw5bKsu2W8FSsMX4mCUerEOxYGgjFujRpkv5sbDl9zbyZl0aCtfdibzS4txAvsbaLl1qhvb9jgj4mBY80oWSd4sPoh~DjFllXGR7PJgze2-bpSCVFhURYE9ZmgFPHLw30KvyUGTbthq5QFy4kJPVAPUH-TcKfWqeA~j-8BMGO6asCcclhuqPkwx38SUo9jZr-Lt~bQM8B9Cj85jubT63THK76c30kzNnhvuY0wOjvVlmIoNGcX0Iqrw8-nqk4M1A__";
+    "/images/pages/who-we-are/section-four/container-bg.jpg";
   return (
     <div
-      className={`relative bg-[url(https://s3-alpha-sig.figma.com/img/d3ed/09f7/252004e28d933592d719d0fca45e5787?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=H0ihIPUscpg9fFeNj~ZI2LnKPv-zvtUsP88b6G4tmrBXkp-4nJXX-pFJ5jAv4PwzwTn2ItkCj99lNhDz9IvVMqdw5bKsu2W8FSsMX4mCUerEOxYGgjFujRpkv5sbDl9zbyZl0aCtfdibzS4txAvsbaLl1qhvb9jgj4mBY80oWSd4sPoh~DjFllXGR7PJgze2-bpSCVFhURYE9ZmgFPHLw30KvyUGTbthq5QFy4kJPVAPUH-TcKfWqeA~j-8BMGO6asCcclhuqPkwx38SUo9jZr-Lt~bQM8B9Cj85jubT63THK76c30kzNnhvuY0wOjvVlmIoNGcX0Iqrw8-nqk4M1A__)] bg-no-repeat before:content-[''] before:absolute before:inset-0 before:bg-gray-900/50 before:w-full before:h-full before:-z-50 z-50 after:content-[''] after:absolute after:bottom-0 after:top-0 after:right-0 after:w-[28%] after:h-full after:bg-[url(https://s3-alpha-sig.figma.com/img/d3ed/09f7/252004e28d933592d719d0fca45e5787?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=H0ihIPUscpg9fFeNj~ZI2LnKPv-zvtUsP88b6G4tmrBXkp-4nJXX-pFJ5jAv4PwzwTn2ItkCj99lNhDz9IvVMqdw5bKsu2W8FSsMX4mCUerEOxYGgjFujRpkv5sbDl9zbyZl0aCtfdibzS4txAvsbaLl1qhvb9jgj4mBY80oWSd4sPoh~DjFllXGR7PJgze2-bpSCVFhURYE9ZmgFPHLw30KvyUGTbthq5QFy4kJPVAPUH-TcKfWqeA~j-8BMGO6asCcclhuqPkwx38SUo9jZr-Lt~bQM8B9Cj85jubT63THK76c30kzNnhvuY0wOjvVlmIoNGcX0Iqrw8-nqk4M1A__)] after:-z-10 after:bg-no-repeat after:bg-center after:bg-cover after:hidden after:md:flex after:bg-black/50`}
+      className={`relative bg-[url(/images/pages/who-we-are/section-four/container-bg.jpg)] bg-no-repeat before:content-[''] before:absolute before:inset-0 before:bg-gray-900/50 before:w-full before:h-full before:-z-50 z-50 after:content-[''] after:absolute after:bottom-0 after:top-0 after:right-0 after:w-[28%] after:h-full after:bg-[url(/images/pages/who-we-are/section-four/container-bg.jpg)] after:-z-10 after:bg-no-repeat after:bg-center after:bg-cover after:hidden after:md:flex after:bg-black/50`}
     >
       <section className="py-14 max-w-6xl mx-auto">
         <CustomVerticalTab gamesOverviews={gamesOverviews} />
---
+++ src/assets/app-data.ts
@@ -0,0 +1,25 @@
+export const appData = {
+  // Home page
+  // Who we are page
+  "whe-we-are": {
+    title: "Who we are",
+    description:
+      "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet.",
+    sections: [
+      {
+        // Section 1
+        headerHero: {
+          bgImage: "",
+          videoUrl: "www.youtube.com/api/vidoes/weqwekf_skadlf_alkfsadfas",
+        },
+        // Section 2
+        sectionTwo: {
+          title: "Where making a differece",
+          description:
+            "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet.",
+          slideImages: ["/images/pages/who-we-are/section-two/slide-img-1.jpg"],
+        },
+      },
+    ],
+  },
+};
