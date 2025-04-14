ef7743e66d3c44414f33ade39f8d7fcfae2c182d
Tausif Ahmed <tausif.ritu1@gmail.com>
2025-04-14 21:29:05 +0600

    feat(auth): implement OTP verification functionality with error handling and navigation

--- src/app/(auth)/verify-otp/_components/verify-otp-form.tsx
+++ src/app/(auth)/verify-otp/_components/verify-otp-form.tsx
@@ -20,6 +20,9 @@ import {
   InputOTPGroup,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import { IOtpToken, IVerifyOTP } from "@/types/redux/auth";
+import { useVerifyOtpMutation } from "@/redux/api/auth.api";
+import { useRouter } from "next/navigation";
 
 const FormSchema = z.object({
   pin: z.string().min(6, {
@@ -35,19 +38,45 @@ export function InputOTPForm() {
     },
   });
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast("You submitted the following values: " + data.pin);
+  const [verifyOTP, {isLoading:verifyOTPPending}] = useVerifyOtpMutation()
+
+  const router = useRouter()
+
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    try {
+      const credentials: IVerifyOTP = {
+        otp: data.pin,
+      };
+
+      const response = await verifyOTP(credentials).unwrap();
+
+      toast.success(response.message || "Successfully logged in");
+      router.push("/");
+    } catch (error: any) {
+      // Handle different error types
+      if (error.message) {
+        toast.error(error.message);
+      } else {
+        toast.error("Ragistration failed. Please try again later");
+      }
+    }
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-[100%] sm:w-3/6 space-y-6">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-[100%] sm:w-3/6 space-y-6"
+      >
         <FormField
           control={form.control}
           name="pin"
           render={({ field }) => (
             <FormItem>
-              <FormLabel className="text-2xl">One-Time Password (OTP)</FormLabel>
+              <FormLabel className="text-2xl">
+                One-Time Password (OTP)
+              </FormLabel>
               <FormControl>
                 <InputOTP maxLength={6} {...field}>
                   <InputOTPGroup>
